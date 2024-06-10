5f105e9230eb1ae6d5834137977fa13eaa89691c
Caique Junior da silva <139816879+CaiqueJrsilva@users.noreply.github.com>
2024-06-10 20:46:09 -0300

    Iniciando o springboot para criação do sistema gerenciar de tarefas

---
+++ GestaoDeTarefas/src/main/java/caique/silva/GestaoDeTarefas/GestaoDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package caique.silva.GestaoDeTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeTarefasApplication.class, args);
+	}
+
+}
---
+++ GestaoDeTarefas/src/test/java/caique/silva/GestaoDeTarefas/GestaoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package caique.silva.GestaoDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
